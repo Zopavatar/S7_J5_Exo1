c37a0584764b92165b99c018d7ff9f73c2828f66
unknown <Charlotte Landuyt>
2023-01-13 09:38:53 +0100

    exo 1

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+let laClass = document.getElementsByClassName("tartine")[0];
+
+console.log(laClass.innerText);
+
+
+let elId = document.getElementById("toast");
+
+console.log(elId.innerText);
+
+
+
+let lesClass = document.getElementsByClassName("tartine");
+let derClass = lesClass[lesClass.length-1];
+
+console.log(derClass);
